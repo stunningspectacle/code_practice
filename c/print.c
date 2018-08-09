d8c080df75c19cafcc49aa312ec5167d7befcbc4
leoyanx <leo.yan@intel.com>
2018-08-10 07:38:29 +0800

    add bashrc
    Signed-off-by: leoyanx <leo.yan@intel.com>

--- c/print.c
+++ c/print.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 void main(void)
 {
 	unsigned long long time = 0x1234567887654321;
+	char str[] = "aaabbbba\n";
 
+	write(STDIN_FILENO, str, sizeof(str));
 	printf("time=0x%llx\n", time);
 
 }

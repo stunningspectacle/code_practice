abaeef74d18e0f31dbb9137afe6eb2138f8932a7
Leo <leoyanx@yahoo.com>
2019-05-07 10:49:09 +0800

    Add build kernel into ubuntu setup
    Signed-off-by: Leo <leoyanx@yahoo.com>

--- setup_ubuntu/setup.sh
+++ setup_ubuntu/setup.sh
@@ -14,23 +14,26 @@ sudo yes | apt-get install --no-install-recommends tmux vim cscope ctags git \
   python3-ply python3-pip python3-setuptools python3-wheel xz-utils file \
   make gcc-multilib autoconf automake libtool libczmq-dev libczmq-dev:i386
 
+# build kernel
+sudo yes | apt-get install flex bison libelf-dev libssl-dev
+
 # Man pages
 sudo yes | apt-get install manpages manpages-de manpages-de-dev manpages-dev \
 	manpages-posix manpages-posix-dev
 # Qemu
-sudo yes | apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager
+#sudo yes | apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager
 
-cd $SETUP_ROOT
-cp .vimrc ~/
-cp -r .vim ~/
-cp .tmux.conf ~/
-cp .gitconf* ~/
-
-if [ "$(grep bashrc_ext ~/.bashrc)" != "" ]; then
-	echo "bashrc extension already exists"
-else
-	echo "bashrc extension doesn't exist, adding..."
-	cat bashrc_ext >> ~/.bashrc
-fi
-
-source ~/.bashrc
+#cd $SETUP_ROOT
+#cp .vimrc ~/
+#cp -r .vim ~/
+#cp .tmux.conf ~/
+#cp .gitconf* ~/
+#
+#if [ "$(grep bashrc_ext ~/.bashrc)" != "" ]; then
+#	echo "bashrc extension already exists"
+#else
+#	echo "bashrc extension doesn't exist, adding..."
+#	cat bashrc_ext >> ~/.bashrc
+#fi
+#
+#source ~/.bashrc

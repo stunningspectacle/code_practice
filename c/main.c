2e42a8505ed69245dde2b00219e3b79b7a4852a7
leoyanx <leo.yan@intel.com>
2019-06-06 23:55:27 +0800

    add some sizeof code int c/
    Signed-off-by: leoyanx <leo.yan@intel.com>

--- c/main.c
+++ c/main.c
@@ -14,6 +14,35 @@ struct num_t {
     char abc;
 };
 
+struct minsize {
+	char a;
+	unsigned long i1;
+	char b;
+};
+
+struct A {
+	char a;
+};
+struct B {
+	char b0;
+	char b1;
+};
+struct C {
+	char c0;
+	int c1;
+	char c2;
+};
+struct D {
+	char d0;
+	unsigned long d1;
+	char d2;
+};
+
+int func0(void)
+{
+	printf("ok");
+}
+
 int main(void)
 {
 	int a = 100;
@@ -30,5 +59,11 @@ int main(void)
 	printf("sizeof(unsigned short)=%d\n", sizeof(unsigned short));
 	printf("sizeof(np->abc)=%d\n", sizeof(np->abc));
 	printf("sizeof(mynum.abc)=%d\n", sizeof(mynum.abc));
+	printf("sizeof(unsigned long)=%d\n", sizeof(unsigned long));
+	printf("main: %p\n", (void *)main);
+	printf("func0: %p\n", (void *)func0);
+	printf("sizeof(minsize): %d\n", sizeof(struct minsize));
+	printf("sizeof A(%d), B(%d), C(%d), D(%d)\n",
+			sizeof(struct A), sizeof(struct B), sizeof(struct C), sizeof(struct D));
 	return 0;
 }
--- c/sizeof.c
+++ c/sizeof.c
@@ -1,16 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+struct A {
+	char a;
+};
+struct B {
+	char b0;
+	char b1;
+};
+struct C {
+	char c0;
+	int c1;
+	char c2;
+};
+struct D {
+	char d0;
+	unsigned long d1;
+	char d2;
+};
+
 int main(void)
 {
 	float f;
 	int a = 10;
 	int b = 3;
 	printf("sizeof(double)=%d\n", sizeof(double));
+	printf("sizeof(long double)=%d\n", sizeof(long double));
 	printf("sizeof(long)=%d\n", sizeof(long));
 	printf("sizeof(long long)=%d\n", sizeof(long long));
 	printf("sizeof(short)=%d\n", sizeof(short));
 	printf("sizeof(short int)=%d\n", sizeof(short int));
+	printf("sizeof A(%d), B(%d), C(%d), D(%d)\n",
+			sizeof(struct A), sizeof(struct B), sizeof(struct C), sizeof(struct D));
 	f = (float)a / b;
 	printf("%f\n", f);
 }
